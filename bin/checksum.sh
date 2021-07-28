6bb614d8d7a5dbace5f3aab44caad95ae0c1296f
punit iam <punitiam@Punits-MacBook-Pro.local>
2021-07-28 16:01:37 +0530

    fixing checksum error

--- bin/checksum.sh
+++ bin/checksum.sh
@@ -3,4 +3,4 @@
 set -eu
 
 # Use `ls | sort` to make checksum to be deterministic since it will be used to choosing the AMI
-cat $(find packer packer/provision.sh packer/template.json terraform/user_data/config.sh.tpl -type f | sort) | md5sum | cut -d ' ' -f 1
+cat $(find packer packer/provision.sh packer/template.json terraform/user-data/config.sh.tpl -type f | sort) | md5sum | cut -d ' ' -f 1
