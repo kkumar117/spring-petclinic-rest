dfe42d9f40a43f16b8b5bc6be9003d3ded0e3e6e
punit iam <punitiam@Punits-MacBook-Pro.local>
2021-07-28 16:50:37 +0530

    fixing checksum error and packer build script

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env sh
+#!bin/bash
 
 set -eu
 
 version=1.0.${CIRCLE_SHA1:-0}
 checksum=${1:-none}
 cd packer
-PACKER_LOG=1 packer build -var "version=$version" -var "checksum=$checksum" template.json
+packer build -var "version=$version" -var "checksum=$checksum" template.json
